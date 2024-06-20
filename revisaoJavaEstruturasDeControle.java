1130a56c919fc82a138e01f25049c94c91405790
orafamoura <raphaelmvb2@gmail.com>
2024-06-20 16:27:01 -0300

    desafioMediaEscolar

--- revisaoJavaEstruturasDeControle.java
+++ revisaoJavaEstruturasDeControle.java
@@ -22,7 +22,9 @@ public class revisaoJavaEstruturasDeControle {
         }
         sc.close();
     }
+
     public static void anoBissexto(String[] args){
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Em que ano estamos: ");
@@ -37,4 +39,27 @@ public class revisaoJavaEstruturasDeControle {
         }
         sc.close();
     }
+
+    public static void mediaEscolar(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Primeira nota: ");
+        double n1 = sc.nextDouble();
+
+        System.out.println("Segunda nota: ");
+        double n2 = sc.nextDouble();
+
+        double mediaFinal = (n1 + n2) / 2;
+
+        if(mediaFinal >= 7.0){
+            System.out.println("Aprovado");
+        } else if(mediaFinal < 7.0 && mediaFinal >= 4.0){
+            System.out.println("Recuperacao");
+        } else {
+            System.out.println("Reprovado");
+        }
+        sc.close();
+    }
+
 }
